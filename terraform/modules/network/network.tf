8549944c199aad79bfb2ae993ed42f9fe864154f
Kledson Basso <kledsonhugo@gmail.com>
2022-11-10 10:55:38 -0300

    Module front SG

--- terraform/modules/network/network.tf
+++ terraform/modules/network/network.tf
@@ -104,46 +104,18 @@ resource "aws_route_table_association" "rt_priv_sn_az2_priv2" {
 
 # RESOURCE: SECURITY GROUPS
 
-resource "aws_security_group" "vpc_sg_pub" {
-    vpc_id = aws_vpc.vpc.id
-    egress {
-        from_port   = "${var.vpc_sg_port_all}"
-        to_port     = "${var.vpc_sg_port_all}"
-        protocol    = "${var.vpc_sg_protocol_any}"
-        cidr_blocks = ["${var.vpc_cidr_all}"]
-    }
-    ingress {
-        from_port   = "${var.vpc_sg_port_all}"
-        to_port     = "${var.vpc_sg_port_all}"
-        protocol    = "${var.vpc_sg_protocol_any}"
-        cidr_blocks = ["${var.vpc_cidr}"]
-    }
-    ingress {
-        from_port   = "${var.vpc_sg_port_ssh}"
-        to_port     = "${var.vpc_sg_port_ssh}"
-        protocol    = "${var.vpc_sg_protocol_tcp}"
-        cidr_blocks = ["${var.vpc_cidr_all}"]
-    }
-    ingress {
-        from_port   = "${var.vpc_sg_port_http}"
-        to_port     = "${var.vpc_sg_port_http}"
-        protocol    = "${var.vpc_sg_protocol_tcp}"
-        cidr_blocks = ["${var.vpc_cidr_all}"]
-    }
-}
-
-resource "aws_security_group" "vpc_sg_priv" {
-    vpc_id = aws_vpc.vpc.id
-    egress {
-        from_port   = "${var.vpc_sg_port_all}"
-        to_port     = "${var.vpc_sg_port_all}"
-        protocol    = "${var.vpc_sg_protocol_any}"
-        cidr_blocks = ["${var.vpc_cidr_all}"]
-    }
-    ingress {
-        from_port   = "${var.vpc_sg_port_all}"
-        to_port     = "${var.vpc_sg_port_all}"
-        protocol    = "${var.vpc_sg_protocol_any}"
-        cidr_blocks = ["${var.vpc_cidr}"]
-    }
-}
+# resource "aws_security_group" "vpc_sg_priv" {
+#     vpc_id = aws_vpc.vpc.id
+#     egress {
+#         from_port   = "${var.vpc_sg_port_all}"
+#         to_port     = "${var.vpc_sg_port_all}"
+#         protocol    = "${var.vpc_sg_protocol_any}"
+#         cidr_blocks = ["${var.vpc_cidr_all}"]
+#     }
+#     ingress {
+#         from_port   = "${var.vpc_sg_port_all}"
+#         to_port     = "${var.vpc_sg_port_all}"
+#         protocol    = "${var.vpc_sg_protocol_any}"
+#         cidr_blocks = ["${var.vpc_cidr}"]
+#     }
+# }
