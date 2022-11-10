f1719eed6b0e56994b1fbd2eec8057a54b5a6bff
Kledson Basso <kledsonhugo@gmail.com>
2022-11-10 12:37:04 -0300

    Module data

--- terraform/modules/data/data.tf
+++ terraform/modules/data/data.tf
@@ -42,6 +42,6 @@ resource "aws_db_instance" "rds_dbinstance" {
     skip_final_snapshot    = "${var.rds_skip_final_snapshot}"
     multi_az               = "${var.rds_multi_az}"
     db_subnet_group_name   = aws_db_subnet_group.rds_sn_group.name
-    parameter_group_name   = aws_db_parameter_group.rds_param_group.name
+#    parameter_group_name   = aws_db_parameter_group.rds_param_group.name
     vpc_security_group_ids = [aws_security_group.data_sg.id]
 }
