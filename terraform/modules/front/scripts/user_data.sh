7330ea53502be50fcb8cfa1760035c8aefa5e282
Kledson Basso <kledsonhugo@gmail.com>
2022-11-11 14:03:56 -0300

    Demo

--- terraform/modules/front/scripts/user_data.sh
+++ terraform/modules/front/scripts/user_data.sh
@@ -3,5 +3,5 @@ yum update -y
 amazon-linux-extras install -y epel
 yum install -y amazon-efs-utils httpd telnet tree
 systemctl enable httpd
-echo "Notifier app frontend" > /mnt/efs/html/index.html
+echo "Notifier app frontend" > /var/www/html/index.html
 service httpd start
